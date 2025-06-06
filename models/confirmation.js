f3052b28aac4ab6f1ace6eb71d60c25c544725bc
vinodkumar0705 <vinodkumarsram@gmail.com>
2025-06-06 14:51:03 +0530

    Changed

--- models/confirmation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const confirmationSchema = new mongoose.Schema({
-  patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
-  doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
-  date: { type: mongoose.Schema.Types.ObjectId, ref: 'Date', required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Confirmation', confirmationSchema);
--- models/date.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const dateSchema = new mongoose.Schema({
-  date: { type: String, required: true },
-  time: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Date', dateSchema);
